7e9886349cf5ab75245bc31242f50e3a44511327
Juan David Ramirez Mendoza <juan.ramirez-me@mail.escuelaing.edu.co>
2018-04-14 21:17:45 -0500

    REST API working

--- target/classes/static/js/api.js
+++ target/classes/static/js/api.js
@@ -1,6 +1,17 @@
 var SquareControllerModule = (function(){
-	var loadResponse = function (){
-		axios.get('/square?value='+document+getElementById('value').value)
-		.then(response)
+	
+	var loadResponse = function () {
+		axios.get('/square?value='+window.location.search.substring(1).split('=')[1])
+		.then(function(response){
+			document.getElementById('response').innerHTML = response.data;
+		})
+		.catch(function(error){
+			console.log(error);
+		});
+		
+	} 
+
+	return {
+		loadResponse: loadResponse
 	};
 })();
